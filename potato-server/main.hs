81e0472b3e2af96518629428b8bf49ed4a0c7908
Bartek Banachewicz <bananu7@o2.pl>
2014-11-13 14:08:00 +0100

    Added generation of water to the map

--- potato-server/main.hs
+++ potato-server/main.hs
@@ -8,12 +8,15 @@ module Main where
 import Potato.Game
 import Potato.Web.App
 import StatefulScotty
-import Control.Lens hiding (index, (.=))
+import Control.Lens hiding (index)
 import Control.Exception (handleJust)
 import System.Environment (getEnv)
 import System.IO.Error (isDoesNotExistError )
 import System.Random
 import Control.Monad.State
+import Control.Applicative
+import Data.Maybe
+import Data.Array ((!))
 
 main = do
     port <- getEnvFallback "PORT" "3000"
@@ -35,20 +38,48 @@ main = do
             | isDoesNotExistError e = Just ()
             | otherwise = Nothing
 
-randomMap :: State StdGen GameMap
-randomMap = do
-    x <- state $ randomR (0, 9)
-    y <- state $ randomR (0, 9)
-    let pos = Point x y
+type RandomM a = State StdGen a
 
-    return $ emptyMap 
+randomMap :: RandomM GameMap
+randomMap = do
+    let map = emptyMap
                 & (ix (Point 0 1).unit) `set` (Just $ Unit 12 Redosia)
                 & (ix (Point 3 4).unit) `set` (Just $ Unit 10 Shitloadnam)
                 & (ix (Point 1 1).city) `set` (Just $ City "Cityville" (Just Redosia))
                 & (ix (Point 8 8).city) `set` (Just $ City "Townville" (Just Shitloadnam))
-                & addRandomCity pos
+    
+    map' <- changeRandomTilesToWater map
+    return map'
+
     where
         addRandomCity pos = (ix pos.city) `set` (Just $ City "Capturetown" Nothing)
+
+        changeRandomTilesToWater :: GameMap -> RandomM GameMap
+        changeRandomTilesToWater m = foldl (>>=) (return m) (replicate 10 changeRandomTileToWater)
+
+        changeRandomTileToWater :: GameMap -> RandomM GameMap
+        changeRandomTileToWater m = do
+            pos <- generateRandomEmptyPoint m
+            return $ m & (ix pos . fieldType) `set` Water
+
+        generateRandomEmptyPoint :: GameMap -> RandomM Point
+        generateRandomEmptyPoint m = do
+            pos <- generateRandomPoint
+            if (valid pos) then return pos
+                           else generateRandomEmptyPoint m
+
+            where
+                valid p = (isNothing $ m ! p ^. unit) &&
+                          (isNothing $ m ! p ^. city) &&
+                          ((== Land) $ m ! p ^. fieldType)
+
+        generateRandomPoint :: RandomM Point
+        generateRandomPoint = do
+            x <- state $ randomR (0, 9)
+            y <- state $ randomR (0, 9)
+            return $ Point x y
+
+
 
 initialMap = emptyMap & (ix (Point 0 1).unit) `set` (Just $ Unit 12 Redosia)
                       & (ix (Point 3 4).unit) `set` (Just $ Unit 10 Shitloadnam)
